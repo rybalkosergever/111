9895b1b0f2825e2b54fff28d8b445c4f95a5262c
rybalkoserg <rybalkoserg@evergreens.com.ua>
2017-02-11 15:35:50 +0200

    Update delete action

--- src/Bookkeeper/ManagerBundle/Controller/BookController.php
+++ src/Bookkeeper/ManagerBundle/Controller/BookController.php
@@ -124,6 +124,23 @@ class BookController extends Controller {
     }
 
     public function deleteAction(Request $request, $id){
+        $delete_form = $this->createFormBuilder()
+            ->setAction($this->generateUrl('book_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete Book'))
+            ->getForm();
 
+        $delete_form->handleRequest($request);
+
+        if($delete_form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $book = $em->getRepository('BookkeeperManagerBundle:Book')->find($id);
+            $em->remove($book);
+            $em->flush();
+        }
+
+        $this->get('session')->getFlashBag()->add('msg', 'Your book has been deleted!');
+
+        return $this->redirect($this->generateUrl('book'));
     }
 }
