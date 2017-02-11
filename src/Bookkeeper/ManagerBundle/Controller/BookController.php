bdb55c833ea36c627bff85cccc50a3bbb4b0fea8
rybalkoserg <rybalkoserg@evergreens.com.ua>
2017-02-11 13:07:43 +0200

    Update createAction method

--- src/Bookkeeper/ManagerBundle/Controller/BookController.php
+++ src/Bookkeeper/ManagerBundle/Controller/BookController.php
@@ -41,6 +41,33 @@ class BookController extends Controller {
 
     public function createAction(Request $request){
 
+        $book = new Book();
+
+        $form = $this->createForm(new BookType(), $book, array(
+            'action' => $this->generateUrl('book_create'),
+            'method' => 'POST'
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create Book'));
+
+        $form->handleRequest($request);
+
+        if($form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->presist($book);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('msg', 'Your book has been created!');
+
+            return $this->redirect($this->generateUrl('book_new'));
+        }
+
+        $this->get('session')->getFlashBag()->add('msg', 'Something went wrong!');
+
+        return $this->render('BookkeeperManagerBundle:Book:new.html.twig', array(
+            'form' => $form->createView()
+        ));
+
     }
 
     public function editAction($id){
