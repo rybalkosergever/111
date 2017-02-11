e1a40743ee58e3c9efcc4bbe1bb4f48b687011d4
rybalkoserg <rybalkoserg@evergreens.com.ua>
2017-02-11 11:03:16 +0200

    2.2. Defining Our Controller

---
+++ src/Bookkeeper/ManagerBundle/Controller/BookController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: serg
+ * Date: 11.02.2017
+ * Time: 10:52
+ */
+
+namespace Bookkeeper\ManagerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class BookController extends Controller {
+
+    public function indexAction(){
+
+    }
+
+    public function showAction($id){
+
+    }
+
+    public function newAction(){
+
+    }
+
+    public function createAction(Request $request){
+
+    }
+
+    public function editAction($id){
+
+    }
+
+    public function updateAction(Request $request, $id){
+
+    }
+
+    public function deleteAction(Request $request, $id){
+
+    }
+}
